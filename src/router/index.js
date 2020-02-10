1adae05a2aa3396a6537955b26ab583b04f45588
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-02-10 09:19:35 +0100

    Remove nav guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import WordsContainer from "../views/WordsContainer.vue";
-import store from "../store";
 
 Vue.use(VueRouter);
-
-const navGuard = next => {
-  if (!store.state.user.isLoggedIn) next({ path: "/" });
-  else next();
-};
 
 const routes = [
   {
@@ -19,23 +13,19 @@ const routes = [
   },
   {
     path: "/words",
-    redirect: "/words/all",
-    beforeEnter: navGuard
+    redirect: "/words/all"
   },
   {
     path: "/words/:category",
-    component: WordsContainer,
-    beforeEnter: navGuard
+    component: WordsContainer
   },
   {
     path: "/user/dashboard",
-    component: () => import("../views/UserPanel.vue"),
-    beforeEnter: navGuard
+    component: () => import("../views/UserPanel.vue")
   },
   {
     path: "/admin",
-    component: () => import("../views/AdminPanel.vue"),
-    beforeEnter: navGuard
+    component: () => import("../views/AdminPanel.vue")
   },
   {
     path: "/register",
